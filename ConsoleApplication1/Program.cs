2cbf48993ba81e1677464d06ea7b9a02d8f8e396
Sabatha <sabatha.luthuli@gmail.com>
2013-07-12 14:56:19 +0200

    Testing Hello World in Visual Studio 2012 using Github extention.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string name;
+            
+            System.Console.WriteLine("Enter your Name: ");
+            name = Convert.ToString(Console.ReadLine());
+            System.Console.WriteLine("Enter your Age: ");
+            age = Convert.ToInt16(Console.ReadLine());
+            System.Console.WriteLine("Hello, World " + name + ".");
+            System.Console.WriteLine("You are " + age + " years old.");
+            Console.Read();
+        
+        }
+    }
+}
